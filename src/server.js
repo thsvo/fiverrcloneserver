1d186a5b92cfd33a53dfe85f818956d6e97f3fdb
thsvo <155262940+thsvo@users.noreply.github.com>
2024-09-30 22:17:04 +0600

    update

--- src/server.js
+++ src/server.js
@@ -1,54 +1,59 @@
-require('dotenv').config();
-const express = require('express');
-const compression = require('compression');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const connect = require('./configs/db');
+require("dotenv").config();
+const express = require("express");
+const compression = require("compression");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const connect = require("./configs/db");
 const PORT = 8080;
 
 // Other Route files
-const { userRoute, conversationRoute, gigRoute, messageRoute, orderRoute, reviewRoute, authRoute } = require('./routes');
+const {
+  userRoute,
+  conversationRoute,
+  gigRoute,
+  messageRoute,
+  orderRoute,
+  reviewRoute,
+  authRoute,
+} = require("./routes");
 
 // App
 const app = express();
 
 // Middlewares
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cookieParser());
-app.use(compression());
-app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:4173', 'https://fiverr-clone-zuhed.netlify.app', 'https://thediscovered.org'],
-    credentials: true
-}));
 
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
 // Other Routes
-app.use('/api/auth', authRoute);
-app.use('/api/users', userRoute);
-app.use('/api/gigs', gigRoute);
-app.use('/api/conversations', conversationRoute);
-app.use('/api/orders', orderRoute);
-app.use('/api/messages', messageRoute);
-app.use('/api/reviews', reviewRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute);
 
 // Routes
-app.get('/', (request, response) => {
-    response.send('Hello, Topper!');
+app.get("/", (request, response) => {
+  response.send("Hello, Topper!");
 });
 
-app.get('/ip', (request, response) => {
-    const list = request.headers['x-forwarded-for'] || request.socket.remoteAddress;
-    const ips = list.split(',');
+app.get("/ip", (request, response) => {
+  const list =
+    request.headers["x-forwarded-for"] || request.socket.remoteAddress;
+  const ips = list.split(",");
 
-    return response.send({ ip: ips[0] });
-})
+  return response.send({ ip: ips[0] });
+});
 
 app.listen(PORT, async () => {
-    try {
-        await connect();
-        console.log(`Listening at http://localhost:${PORT}`);
-    }
-    catch ({ message }) {
-        console.log(message);
-    }
-})
+  try {
+    await connect();
+    console.log(`Listening at http://localhost:${PORT}`);
+  } catch ({ message }) {
+    console.log(message);
+  }
+});
